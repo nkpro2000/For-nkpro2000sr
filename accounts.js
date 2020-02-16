125dae6638ae2a450aa299986b07b856404e93ef
nkpro2000 <45198561+nkpro2000@users.noreply.github.com>
2020-02-16 17:29:21 +0530

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,1 +1,3 @@
-$.getScript("https://gist.github.com/nkpro2000sr/6e501afdf929b59effa271b0451088b4.js")
+var url = "https://gist.github.com/nkpro2000sr/6e501afdf929b59effa271b0451088b4.js"
+var scode = $.get(url,dataType="text")
+document.write(scode)
